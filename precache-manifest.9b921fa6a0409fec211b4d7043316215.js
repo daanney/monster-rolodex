aabd93b3f549fa01d9a1ec18119007db7bae3035
danney <danney@macbook.local>
2020-04-30 18:00:14 +0200

    Updates

---
+++ precache-manifest.9b921fa6a0409fec211b4d7043316215.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993f3075376dc919120f51ac5630b957",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e94aa94775554ae0020a",
+    "url": "/monster-rolodex/static/css/main.d8cf2ba8.chunk.css"
+  },
+  {
+    "revision": "98e12b2649aad55b1a33",
+    "url": "/monster-rolodex/static/js/2.e2ed609f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.e2ed609f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94aa94775554ae0020a",
+    "url": "/monster-rolodex/static/js/main.3d58c238.chunk.js"
+  },
+  {
+    "revision": "1c79305e130eba318f28",
+    "url": "/monster-rolodex/static/js/runtime-main.5ec4cad5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
